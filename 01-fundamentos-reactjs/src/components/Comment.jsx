aeca2bd120ecac82bc3f3d8a34efd8ef73f77f63
Elisa Pessamilio <elisa.cardoso.de.mello@gmail.com>
2024-10-04 14:02:03 -0300

    style: criando lista de comentários

--- 01-fundamentos-reactjs/src/components/Comment.jsx
+++ 01-fundamentos-reactjs/src/components/Comment.jsx
@@ -1,7 +1,34 @@
+import { ThumbsUp, Trash } from 'phosphor-react'
 import styles from './Comment.module.css'
 
 export function Comment(){
     return(
-        <></>
+        <div className={styles.comment}>
+            <img src='https://images.unsplash.com/photo-1638803040283-7a5ffd48dad5?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'/>
+            
+            <div className={styles.commentBox}>
+                <div className={styles.commentContent}>
+                    <header>
+                        <div className={styles.authorAndTime}>
+                            <strong>Lucas Pessamilio</strong>
+                            <time title='11 de Maio às 8:13h' dateTime='2022-05-11 08:13:30'>Cerca de 1h atrás</time>
+                        </div>
+
+                        <button title='Deletar comentário'>
+                            <Trash size={20}/>
+                        </button>
+                    </header>
+
+                    <p>Muito bom Elisa, parabéns!! 👏👏</p>
+                </div>
+
+                <footer>
+                    <button>
+                        <ThumbsUp size={24}/>
+                        Aplaudir <span>20</span>
+                    </button>
+                </footer>
+            </div>
+        </div>
     )
 }
--- 01-fundamentos-reactjs/src/components/Post.jsx
+++ 01-fundamentos-reactjs/src/components/Post.jsx
@@ -1,3 +1,4 @@
+import { Comment } from './Comment';
 import styles from './Post.module.css';
 
 export function Post() {
@@ -32,13 +33,17 @@ export function Post() {
                 <textarea
                     placeholder='Deixe um comentário'
                 />
-                
+
                 <footer>
                     <button type='submit'>
                         Publicar
                     </button>
                 </footer>
             </form>
+
+            <div className={styles.commentList}>
+                <Comment/>
+            </div>
         </article>
     )
 }
