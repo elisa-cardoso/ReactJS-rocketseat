fefe208b91fba6c563c887374fd86081e031af69
Elisa Pessamilio <elisa.cardoso.de.mello@gmail.com>
2024-10-09 14:24:32 -0300

    style: modal de saida e entrada

--- 03-dt-money/src/pages/Transactions/styles.ts
+++ 03-dt-money/src/pages/Transactions/styles.ts
@@ -35,5 +35,5 @@ interface PriceHighlightProps {
 
 
 export const PriceHighlight = styled.span<PriceHighlightProps>`
-color: ${props => props.variant == 'income' ? props.theme['green-300'] : props.theme['red-300']};
+color: ${props => props.variant == 'income' ? props.theme['green-300'] : props.theme['red-500']};
 `
